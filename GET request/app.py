21087b137b5515e0112ade87f4a7e591b86bec08
Mohammad Adnan <mohmmadadnan930@gmail.com>
2026-05-16 15:31:38 +0530

    Add files via upload

---
+++ GET request/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def load():
+    return "go to '/square' route to find the square of a number." 
+
+@app.route('/square', methods=['GET'])
+def squarenumber():
+    num = request.args.get('num')
+
+    if num is None:  # No number entered, show input form
+        return render_template('index.html')
+    elif num.strip() == '':  # Empty input
+        return "<h1>Invalid number. Please enter a number.</h1>"
+    try:
+        square = int(num) ** 2
+        return render_template('answer.html', squareofnum=square, num=num)
+    except ValueError:
+        return "<h1>Invalid input. Please enter a valid number.</h1>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
